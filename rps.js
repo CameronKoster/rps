db42067951302584dbda1800b4f9276dedcb50f7
Cameron Koster <kostercam37@gmail.com>
2018-11-01 14:47:02 -0600

    done af

--- rps.js
+++ rps.js
@@ -1,48 +1,50 @@
+let computerChoices = ["rock", "paper", "scissors"]
+
+
 function play(playerChoice) {
 
+  let computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  let results = ""
 
-  //rock
-  let computersChoiceRock = "rock";
-  if (playerChoice == "paper") {
-    return (alert("WIN!!!"))
-  }; 
-  else if (playerChoice == "scissors") {
-    return (alert("LOSE!!!"))
-  };
-  else (playerChoice == "rock") {
-    return (alert("DRAW!!!"))
-  };
+  if (playerChoice() == "paper") {
+    if (computerChoice == "rock") {
+      results = "WIN!!!"
+    }
+    else if (computerChoice == "scissors") {
+      results = "LOSE!!!"
+    }
+    else {
+      results = "DRAW!!!"
+    }
+  }
 
 
 
-  //paper
-  let computersChoicePaper = "paper";
   if (playerChoice == "scissors") {
-    return (alert("WIN!!!"))
-  }; 
-  else if (playerChoice == "rock") {
-    return (alert("LOSE!!!"))
-  };
-  else (playerChoice == "paper") {
-    return (alert("DRAW!!!"))
-  };
+    if (computerChoice == "paper") {
+      results = "WIN!!!"
+    }
+    else if (computerChoice == "rock") {
+      results = "LOSE!!!"
+    }
+    else {
+      results = "DRAW!!!"
+    }
+  }
 
 
 
-  //scissor
-  let computersChoiceScissors = "scissors";
+
   if (playerChoice == "rock") {
-    return (alert("WIN!!!"))
-  }; 
-  else if (playerChoice == "rock") {
-    return (alert("LOSE!!!"))
-  };
-  else (playerChoice == "scissors") {
-    return (alert("DRAW!!!"))
-  };
-
-
-
-
-
+    if (computerChoice == "scissors") {
+      results = "WIN!!!"
+    }
+    else if (computerChoice == "rock") {
+      results = "LOSE!!!"
+    }
+    else {
+      results = "DRAW!!!"
+    }
+  }
+  document.getElementById('words').innerText = results
 }
